cd27e7027f32cd111996c4bdd5ac17d3f40c9990
Roberto Stagi <robertostagi@MBP-di-Roberto.homenet.telecomitalia.it>
2021-11-30 23:34:54 +0100

    add brew and zsh

---
+++ TODOs.md
@@ -0,0 +1,4 @@
+# List of TODO stuff
+
+- Change behaviour of install_brew.sh in order to avoid manual keyboard input
+
---
+++ install_brew.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+
