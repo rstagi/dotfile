73266677a70b730898253eadd2c4090fc85689c8
rstagi <rsta@bendingspoons.com>
2023-01-30 09:59:27 +0100

    feat(init_zsh): add additional tools

--- init_zsh.sh
+++ init_zsh.sh
@@ -1,5 +1,7 @@
 #/bin/zsh
 
+brew install tree ack fzf shellcheck trash-cli
+$(brew --prefix)/opt/fzf/install
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
 # TODO ask if the user wants to override the .zshrc or not
