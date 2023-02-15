90bff157183179d1df5dd81104011e47b92cba5e
rstagi <rsta@bendingspoons.com>
2023-02-15 19:55:16 +0100

    chore: add in progress comments on scripts

--- init_zsh.sh
+++ init_zsh.sh
@@ -1,5 +1,7 @@
 #/bin/zsh
 
+### IN PROGRESS ###
+
 brew install tree ack fzf shellcheck trash-cli
 $(brew --prefix)/opt/fzf/install
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
